36423dfd62fe29eb1b67b9d4fd5869f3e70f5246
Riley Robins <rileyrobins@live.ca>
2013-11-22 20:51:41 -0600

    Active Admin up and running

--- app/models/province.rb
+++ app/models/province.rb
@@ -3,6 +3,7 @@ class Province < ActiveRecord::Base
   has_many :customer
 
   validates :gst, :pst, :hst, :name, presence: true
+  validates :name, uniqueness: true
 
   attr_accessible :gst, :hst, :name, :pst
 end
