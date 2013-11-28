ca5ff3636395682e544e42904d84ee27b0000a64
Riley Robins <rileyrobins@live.ca>
2013-11-28 13:02:23 -0600

    category list rendering on index

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,7 +1,7 @@
 class MainController < ApplicationController
   def index
   	@products = Product.order("name")
-  	@categories = Category.order("id")
+  	@categories = Category.all
   end
 
   def category
